e703603c5c2ce6fb098c2a25c7dc54c9e2d9e9aa
Ian M <44310933+mnai01@users.noreply.github.com>
2021-10-26 21:53:03 -0400

    FIxed layout, added basic temp mock map

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import Map from "../components/Map";
+import Map from '../components/Map/index.jsx';
 const Home: React.FC = () => {
-  return (
-    <div>
-      <Map />
-    </div>
-  );
+    return (
+        <div>
+            <Map />
+        </div>
+    );
 };
 
 export default Home;
